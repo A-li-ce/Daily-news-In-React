2a816e8ff9b0ea25f78718bba679aa2909a1f1c7
A-li-ce <aliceraj3012@gmail.com>
2023-12-12 00:29:00 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,20 @@ import Spinner from './components/Spinner'
 const App = ()=>{ 
 
     let pageSize = 20
-    // const apiKey = import.meta.env.VITE_NEWS_API
+    const apiKey = import.meta.env.VITE_NEWS_API
+
     return (
       <div >
         <Router>
             <Navbar/>
             <Routes>
-              <Route exact path="/"element = {<News key = "general"  pageSize={pageSize} country='in' category = "general"/>} />
-              <Route exact path="/business"element = {<News key = "business"  pageSize={pageSize} country='in' category = "business"/>} />
-              <Route exact path="/entertainment"element = {<News key = "entertainment"   pageSize={pageSize} country='in' category = "entertainment"/>}/>
-              <Route exact path="/health"element = {<News key = "health"  pageSize={pageSize} country='in' category = "health"/>}/>
-              <Route exact path="/science"element = {<News key = "science"  pageSize={pageSize} country='in' category = "science"/>}/>
-              <Route exact path="/sports"element = {<News key = "sports"  pageSize={pageSize} country='in' category = "sports"/>} />
-              <Route exact path="/technology"element = {<News key = "technology"  pageSize={pageSize} country='in' category = "technology"/>}/>
+              <Route exact path="/"element = {<News key = "general" apiKey ={apiKey} pageSize={pageSize} country='in' category = "general"/>} />
+              <Route exact path="/business"element = {<News key = "business" apiKey ={apiKey} pageSize={pageSize} country='in' category = "business"/>} />
+              <Route exact path="/entertainment"element = {<News key = "entertainment" apiKey ={apiKey} pageSize={pageSize} country='in' category = "entertainment"/>}/>
+              <Route exact path="/health"element = {<News key = "health" apiKey ={apiKey} pageSize={pageSize} country='in' category = "health"/>}/>
+              <Route exact path="/science"element = {<News key = "science" apiKey ={apiKey} pageSize={pageSize} country='in' category = "science"/>}/>
+              <Route exact path="/sports"element = {<News key = "sports" apiKey ={apiKey} pageSize={pageSize} country='in' category = "sports"/>} />
+              <Route exact path="/technology"element = {<News key = "technology" apiKey ={apiKey} pageSize={pageSize} country='in' category = "technology"/>}/>
             </Routes>
 
         </Router>
